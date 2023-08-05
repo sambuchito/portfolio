f87a3a57e06259254e42ff054ab0ad68479d23bf
sambuchito <lulangar@gmail.com>
2023-08-04 21:41:51 -0300

    segundo intento

---
+++ ALL/JS/app.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  } 
+
+  function clic(){
+    alert("Hola!, como carancho estas?");
+}
+
+saludar();
